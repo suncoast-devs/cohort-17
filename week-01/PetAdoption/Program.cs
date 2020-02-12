c4c4ede6243943c7a5bc2a2464d82ef39d4e2f14
Mark Dewey <mark.t.dewey@gmail.com>
2020-02-12 11:26:51 -0500

    can add pets to shelter

---
+++ week-01/PetAdoption/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetAdoption
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var shelter = new List<string>();
+
+
+      while (true)
+      {
+
+        Console.WriteLine("Current Pets");
+        Console.WriteLine($"There are {shelter.Count} pets.");
+        for (var i = 0; i <= shelter.Count; i++)
+        {
+          var currentPet = shelter[i];
+          Console.WriteLine($"{i} : {currentPet}");
+        }
+
+        Console.WriteLine("What pet do you want to add?");
+        var pet = Console.ReadLine();
+
+        shelter.Add(pet);
+      }
+
+
+    }
+  }
+}
---
+++ week-01/PetAdoption/README.md
@@ -0,0 +1,5 @@
+Requirements:
+
+- As a user i want see all pets in the shelter
+- as a user I want to add pet to the shelter, we found kitties!
+- as a user I want to adopt a pet from the shelter (delete)
