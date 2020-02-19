2c244459967094b9b728a7beb302ca486570df3f
Mark Dewey <mark.t.dewey@gmail.com>
2020-02-19 10:30:11 -0500

    Started Project, Added Sightings Class

---
+++ week-02/CryptidTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptidTracker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
---
+++ week-02/CryptidTracker/Sightings.cs
@@ -0,0 +1,16 @@
+
+
+using System;
+
+namespace CryptidTracker
+{
+  public class Sightings
+  {
+    // WhatISaw
+    public string WhatISaw { get; set; }
+    // WhereISawIt
+    public string WhereISawIt { get; set; }
+    // WhenISawIt
+    public DateTime WhenISawIt { get; set; }
+  }
+}
