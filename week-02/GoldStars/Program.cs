558b823a5ae0905b5f7de158ed3449be4c33972a
Mark Dewey <mark.t.dewey@gmail.com>
2020-02-20 12:02:00 -0500

    added csv writer

--- week-02/GoldStars/Program.cs
+++ week-02/GoldStars/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using CsvHelper;
 
 namespace GoldStars
 {
   class Program
   {
+
+    static void SaveStudents(List<Student> students)
+    {
+      var writer = new StreamWriter("students.csv");
+      var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+      csvWriter.WriteRecords(students);
+      writer.Flush();
+    }
+
+
     static void Main(string[] args)
     {
       Console.WriteLine("Welcome to GoldStars!");
@@ -21,16 +33,25 @@ namespace GoldStars
       var students = new List<Student>();
       // read file that contains students
 
-      var reader = new StreamReader("students.csv");
-      var data = reader.ReadToEnd();
 
-      // split
-      // convert
-      var studentNames = data.Split("\n");
-      students = studentNames
-          .Where(name => name != "")
-          .Select(name => new Student { Name = name })
-          .ToList();
+      // read without Package
+      // var reader = new StreamReader("students.csv");
+      // var data = reader.ReadToEnd();
+
+      // // split
+      // // convert
+      // var studentNames = data.Split("\n");
+      // students = studentNames
+      //     .Where(name => name != "")
+      //     .Select(name => new Student { Name = name })
+      //     .ToList();
+
+      // read with Package 
+
+      var reader = new StreamReader("students.csv");
+      var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+      students = csvReader.GetRecords<Student>().ToList();
+
 
       var isRunning = true;
       while (isRunning)
@@ -52,6 +73,7 @@ namespace GoldStars
             Name = name
           };
           students.Add(student);
+          SaveStudents(students);
         }
         else if (input == "give")
         {
@@ -59,17 +81,28 @@ namespace GoldStars
           var name = Console.ReadLine();
           var student = students.First(student => student.Name == name);
           student.Stars++;
+          SaveStudents(students);
+        }
+        else if (input == "clear")
+        {
+          SaveStudents(new List<Student>());
         }
         else if (input == "quit")
         {
           isRunning = false;
           // save students to a file
-          var writer = new StreamWriter("students.csv");
-          foreach (var student in students)
-          {
-            writer.WriteLine($"{student.Name}, {student.Stars}");
-          }
-          writer.Flush();
+
+          // without Package
+          // var writer = new StreamWriter("students.csv");
+          // foreach (var student in students)
+          // {
+          //   writer.WriteLine($"{student.Name}, {student.Stars}");
+          // }
+          // writer.Flush();
+
+          // with Package
+          SaveStudents(students);
+
 
         }
       }
