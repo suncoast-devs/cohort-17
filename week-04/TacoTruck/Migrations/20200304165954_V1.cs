ea32cc70b39f03c496c946426a78131d42230a0c
Mark Dewey <mark.t.dewey@gmail.com>
2020-03-04 12:09:24 -0500

    reset database imigrations

---
+++ week-04/TacoTruck/Migrations/20200304165954_V1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TacoTruck.Migrations
+{
+    public partial class V1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MenuItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true),
+                    IsVegan = table.Column<bool>(nullable: false),
+                    Calories = table.Column<int>(nullable: false),
+                    Protein = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trucks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Mileage = table.Column<int>(nullable: false),
+                    Color = table.Column<string>(nullable: true),
+                    IsCash = table.Column<bool>(nullable: false),
+                    FirstDayOfService = table.Column<DateTime>(nullable: false),
+                    LastDayOfService = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trucks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TruckMenu",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MenuItemId = table.Column<int>(nullable: false),
+                    TruckId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TruckMenu", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TruckMenu_MenuItems_MenuItemId",
+                        column: x => x.MenuItemId,
+                        principalTable: "MenuItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TruckMenu_Trucks_TruckId",
+                        column: x => x.TruckId,
+                        principalTable: "Trucks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TruckMenu_MenuItemId",
+                table: "TruckMenu",
+                column: "MenuItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TruckMenu_TruckId",
+                table: "TruckMenu",
+                column: "TruckId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TruckMenu");
+
+            migrationBuilder.DropTable(
+                name: "MenuItems");
+
+            migrationBuilder.DropTable(
+                name: "Trucks");
+        }
+    }
+}
