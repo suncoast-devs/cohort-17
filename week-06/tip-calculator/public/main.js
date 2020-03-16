80259fa807c9b5a858cc6881d003c48b15834485
Mark Dewey <mark.t.dewey@gmail.com>
2020-03-16 11:45:10 -0400

    added refactor

--- week-06/tip-calculator/public/main.js
+++ week-06/tip-calculator/public/main.js
@@ -2,14 +2,18 @@ const main = () => {
   console.log('Hello, World')
 }
 
-const calculateTip = () => {
+const sum = (x, y) => {
+  return x + y
+}
+
+const calculateTip = tipAmount => {
   // this is where our code will actually go
-  console.log('20% button clicked')
+  console.log(tipAmount + ' button clicked')
   // get the total from the input box
   const total = document.querySelector('input').value
   console.log(total)
   // calculate a 20% tip
-  const tip = total * 0.2
+  const tip = total * tipAmount
   // add the tip to the total
   const billTotal = parseFloat(total) + tip
   // total += tip
@@ -18,8 +22,26 @@ const calculateTip = () => {
   document.querySelector('.total').textContent = '$' + billTotal.toFixed(2)
 }
 
+const calculate20Tip = () => {
+  calculateTip(0.2)
+}
+
+const calculate15Tip = () => {
+  calculateTip(0.15)
+}
+
+const calculate18Tip = () => {
+  calculateTip(0.18)
+}
+
 document.addEventListener('DOMContentLoaded', main)
 // Add an event listener
 document
   .querySelector('.twentyPercentTip')
-  .addEventListener('click', calculateTip)
+  .addEventListener('click', calculate20Tip)
+document
+  .querySelector('.fifteenPercentTip')
+  .addEventListener('click', calculate15Tip)
+document
+  .querySelector('.eighteenPercentTip')
+  .addEventListener('click', calculate18Tip)
