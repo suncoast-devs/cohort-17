5903b73c00e5dc5631366c18feaa0e0cc1a6c2e4
Mark Dewey <mark.t.dewey@gmail.com>
2020-03-17 10:24:59 -0400

    addedd basic RPS logic for rock

--- week-06/rock-paper-scissors/public/main.js
+++ week-06/rock-paper-scissors/public/main.js
@@ -1,7 +1,47 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const main = () => {}
+
+const playerSelectedRock = () => {
+  console.log('player selected rock')
+  const options = ['rock', 'paper', 'scissors']
+  // pick a random selection
+  const randomNumber = Math.floor(Math.random() * options.length)
+  const cpuPick = options[randomNumber]
+
+  console.log(cpuPick)
+  if (cpuPick === 'scissors') {
+    console.log('rock beats scissors')
+    document.querySelector('.game-result').textContent =
+      'Rock beats scissors, player wins'
+  } else if (cpuPick === 'rock') {
+    console.log('tied')
+    document.querySelector('.game-result').textContent =
+      'Rock ties rock, tie game'
+  } else if (cpuPick === 'paper') {
+    console.log('paper beats rock, cpu wins')
+    document.querySelector('.game-result').textContent =
+      'Paper beats rocks, cpu wins'
   }
+
+  // display the player selection
+  // clear out result
+  document.querySelector('.player-choice').textContent = ''
+  // create a new <img>
+  const imgTag = document.createElement('img')
+  // set the src
+  imgTag.src = '/images/rock.png'
+  // add the image to the DOM
+  document.querySelector('.player-choice').appendChild(imgTag)
+  // display the cpu selection
+  const parent = document.querySelector('.cpu-choice')
+  parent.textContent = ''
+  const cpuImgTag = document.createElement('img')
+  // cpuImgTag.src = '/images/' + cpuPick + '.png'
+  cpuImgTag.src = `/images/${cpuPick}.png`
+  parent.appendChild(cpuImgTag)
+  // display who won
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document
+  .querySelector('.rock-button')
+  .addEventListener('click', playerSelectedRock)
