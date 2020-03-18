190a116b0015481746f3fb0cad58f29db747f594
Mark Dewey <mark.t.dewey@gmail.com>
2020-03-18 12:53:48 -0400

    added some more refactors

--- week-06/monster-mania/src/components/NotableMonsters.js
+++ week-06/monster-mania/src/components/NotableMonsters.js
@@ -7,13 +7,21 @@ import wolfman from '../images/wolfman.jpg'
 
 export class NotableMonsters extends Component {
   render() {
+    const monsters = [
+      { name: 'Wolfman', url: wolfman },
+      { name: 'Nosferatu', url: nosferatu },
+      { name: 'Bride of Frankenstein', url: bride },
+    ]
     return (
       <section class="notable-monsters">
         <header>Notable Others</header>
         <ul>
-          <Monster monsterName="Wolfman" imageUrl={wolfman} />
+          {monsters.map(mon => {
+            return <Monster monsterName={mon.name} imageUrl={mon.url} />
+          })}
+          {/* <Monster monsterName="Wolfman" imageUrl={wolfman} />
           <Monster monsterName="Nosferatu" imageUrl={nosferatu} />
-          <Monster imageUrl={bride} monsterName="Bride of Frankenstein" />
+          <Monster imageUrl={bride} monsterName="Bride of Frankenstein" /> */}
         </ul>
       </section>
     )
