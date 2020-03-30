a897553a27547d6dbe124b45f37730dcba6a88f8
Mark Dewey <mark.t.dewey@gmail.com>
2020-03-30 12:04:40 -0400

    added trail page

--- week-08/HikeFinder/ClientApp/src/App.jsx
+++ week-08/HikeFinder/ClientApp/src/App.jsx
@@ -6,6 +6,7 @@ import Search from './pages/Search'
 import NotFound from './pages/NotFound'
 import AddTrail from './pages/AddTrail'
 import './custom.css'
+import TrailDetails from './pages/TrailDetails'
 
 export default class App extends Component {
   static displayName = App.name
@@ -17,6 +18,7 @@ export default class App extends Component {
           <Route exact path="/" component={Home} />
           <Route exact path="/search" component={Search} />
           <Route exact path="/add" component={AddTrail} />
+          <Route exact path="/trail/:trailId" component={TrailDetails} />
           <Route exact path="*" component={NotFound} />
         </Switch>
       </Layout>
--- week-08/HikeFinder/ClientApp/src/pages/AddTrail.jsx
+++ week-08/HikeFinder/ClientApp/src/pages/AddTrail.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 const AddTrail = () => {
   const [trail, setTrail] = useState({})
+  const [wasSuccessfullyCreated, setWasSuccessfullyCreated] = useState({
+    shouldRedirect: false,
+    newTrailInformation: {},
+  })
 
   const updateTrialData = e => {
     const key = e.target.name
@@ -18,46 +23,61 @@ const AddTrail = () => {
     const resp = await axios.post('/api/trails', trail)
     if (resp.status === 201) {
       // do something something else
+      setWasSuccessfullyCreated({
+        shouldRedirect: true,
+        newTrailInformation: resp.data,
+      })
     } else {
       // do something else here
     }
   }
 
-  return (
-    <>
-      <main>
-        <section>
-          <label htmlFor="">Name</label>
-          <input type="text" name="name" onChange={updateTrialData} />
-        </section>
-        <section>
-          <label htmlFor="">Description</label>
-          <input type="text" name="description" onChange={updateTrialData} />
-        </section>
-        <section>
-          <label htmlFor="">Grade</label>
-          <input type="text" name="grade" onChange={updateTrialData} />
-        </section>
-        <section>
-          <label htmlFor="">Route Type</label>
-          <input type="text" name="routeType" onChange={updateTrialData} />
-        </section>
-        <section>
-          <label htmlFor="">Address</label>
-          <input type="text" name="address" onChange={updateTrialData} />
-        </section>
-        <section>
-          <label htmlFor="">City</label>
-          <input type="text" name="city" onChange={updateTrialData} />
-        </section>
-        <section>
-          <label htmlFor="">State</label>
-          <input type="text" name="state" onChange={updateTrialData} />
-        </section>
-        <button onClick={addTrailToApi}>Add trail</button>
-      </main>
-    </>
-  )
+  if (wasSuccessfullyCreated.shouldRedirect) {
+    return (
+      <Redirect
+        to={{
+          pathname: `/trail/${wasSuccessfullyCreated.newTrailInformation.id}`,
+          state: { trail: wasSuccessfullyCreated.newTrailInformation },
+        }}
+      />
+    )
+  } else {
+    return (
+      <>
+        <main>
+          <section>
+            <label htmlFor="">Name</label>
+            <input type="text" name="name" onChange={updateTrialData} />
+          </section>
+          <section>
+            <label htmlFor="">Description</label>
+            <input type="text" name="description" onChange={updateTrialData} />
+          </section>
+          <section>
+            <label htmlFor="">Grade</label>
+            <input type="text" name="grade" onChange={updateTrialData} />
+          </section>
+          <section>
+            <label htmlFor="">Route Type</label>
+            <input type="text" name="routeType" onChange={updateTrialData} />
+          </section>
+          <section>
+            <label htmlFor="">Address</label>
+            <input type="text" name="address" onChange={updateTrialData} />
+          </section>
+          <section>
+            <label htmlFor="">City</label>
+            <input type="text" name="city" onChange={updateTrialData} />
+          </section>
+          <section>
+            <label htmlFor="">State</label>
+            <input type="text" name="state" onChange={updateTrialData} />
+          </section>
+          <button onClick={addTrailToApi}>Add trail</button>
+        </main>
+      </>
+    )
+  }
 }
 
 export default AddTrail
