5c0e14bce63be2b01265773158edc48b46b497c2
Mark Dewey <mark.t.dewey@gmail.com>
2020-04-01 14:50:35 -0400

    ready for pig latin

---
+++ week-08/PigLatin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PigLatin
+{
+  class Program
+  {
+
+    static string Translate(string sentence)
+    {
+      return "";
+    }
+
+
+    static void Main(string[] args)
+    {
+      // Console.WriteLine("Welcome to C#");
+      Console.WriteLine(Translate("hello") == "ellohay");
+      Console.WriteLine(Translate("hello world") == "ellohay orldway");
+      Console.WriteLine(Translate("Hello World") == "Ellohay Orldway");
+      Console.WriteLine(Translate("Pizza? Yes Please!!") == "Izzapay? Esyay Easeplay!!");
+      Console.WriteLine(Translate("How are you?") == "Owhay areway ouyay?");
+    }
+  }
+}
