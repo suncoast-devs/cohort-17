f802fc4ce94177abb940089bd62a6e64a9a7a8c4
Mark Dewey <mark.t.dewey@gmail.com>
2020-04-01 11:19:34 -0400

    saving reviews

--- week-08/HikeFinder/ClientApp/src/components/Trail.jsx
+++ week-08/HikeFinder/ClientApp/src/components/Trail.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Trail = props => {
   const { trail } = props
+  const [newReviewText, setNewReviewText] = useState('')
+  const [reviewScore, setReviewScore] = useState(0)
+
+  const sendReviewToApi = async () => {
+    const resp = await axios.post(`/api/trails/${trail.id}/reviews`, {
+      rating : reviewScore,
+      comment: newReviewText,
+    })
+    console.log(resp.data)
+  }
+
   return (
     <main className="trail-details">
       <img src="https://placekitten.com/600/400" alt={trail.name} />
@@ -29,14 +40,22 @@ const Trail = props => {
           cols="30"
           rows="10"
           placeholder="leave a review"
+          value={newReviewText}
+          onChange={e => setNewReviewText(e.target.value)}
         ></textarea>
         <h2>rate it!</h2>
-        <button>1</button>
-        <button>3</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+
+        <button onClick={() => setReviewScore(1)}>1</button>
+        <button onClick={() => setReviewScore(2)}>2</button>
+        <button onClick={() => setReviewScore(3)}>3</button>
+        <button onClick={() => setReviewScore(4)}>4</button>
+        <button onClick={() => setReviewScore(5)}>5</button>
+        <h3>your score : {reviewScore}/5</h3>
+        <div>
+          <button onClick={sendReviewToApi}>Add Review</button>
+        </div>
         <h2>Reviews</h2>
+
         <ul className="review-list">
           <li>
             <p>
--- week-08/HikeFinder/Controllers/TrailsController.cs
+++ week-08/HikeFinder/Controllers/TrailsController.cs
@@ -9,101 +9,120 @@ using HikeFinder.Models;
 
 namespace HikeFinder.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TrailsController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TrailsController : ControllerBase
+  {
+    private readonly DatabaseContext _context;
+
+    public TrailsController(DatabaseContext context)
     {
-        private readonly DatabaseContext _context;
-
-        public TrailsController(DatabaseContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Trails
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Trail>>> GetTrails()
-        {
-            return await _context.Trails.ToListAsync();
-        }
-
-        // GET: api/Trails/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Trail>> GetTrail(int id)
-        {
-            var trail = await _context.Trails.FindAsync(id);
-
-            if (trail == null)
-            {
-                return NotFound();
-            }
-
-            return trail;
-        }
-
-        // PUT: api/Trails/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTrail(int id, Trail trail)
-        {
-            if (id != trail.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(trail).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TrailExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Trails
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<Trail>> PostTrail(Trail trail)
-        {
-            _context.Trails.Add(trail);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTrail", new { id = trail.Id }, trail);
-        }
-
-        // DELETE: api/Trails/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Trail>> DeleteTrail(int id)
-        {
-            var trail = await _context.Trails.FindAsync(id);
-            if (trail == null)
-            {
-                return NotFound();
-            }
-
-            _context.Trails.Remove(trail);
-            await _context.SaveChangesAsync();
-
-            return trail;
-        }
-
-        private bool TrailExists(int id)
-        {
-            return _context.Trails.Any(e => e.Id == id);
-        }
+      _context = context;
     }
+
+    // GET: api/Trails
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Trail>>> GetTrails()
+    {
+      return await _context.Trails.ToListAsync();
+    }
+
+    // GET: api/Trails/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Trail>> GetTrail(int id)
+    {
+      var trail = await _context.Trails.FindAsync(id);
+
+      if (trail == null)
+      {
+        return NotFound();
+      }
+
+      return trail;
+    }
+
+    // PUT: api/Trails/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+    // more details see https://aka.ms/RazorPagesCRUD.
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTrail(int id, Trail trail)
+    {
+      if (id != trail.Id)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(trail).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!TrailExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/Trails
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+    // more details see https://aka.ms/RazorPagesCRUD.
+    [HttpPost]
+    public async Task<ActionResult<Trail>> PostTrail(Trail trail)
+    {
+      _context.Trails.Add(trail);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetTrail", new { id = trail.Id }, trail);
+    }
+
+    [HttpPost("{trailId}/reviews")]
+    public async Task<ActionResult> AddReviewForTrail(int trailId, Review review)
+    {
+      //opt 1
+      // adding review to the trial
+      review.TrailId = trailId;
+      // add the review to the database
+      _context.Reviews.Add(review);
+      await _context.SaveChangesAsync();
+      // returning something
+      return Ok(review);
+      //   opt 2
+
+      //   var trail = await _context.Trails.FirstOrDefaultAsync(f => f.Id == trailId);
+      //   trail.Reviews.Add(review);
+      //   await _context.SaveChangesAsync();
+
+    }
+
+    // DELETE: api/Trails/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Trail>> DeleteTrail(int id)
+    {
+      var trail = await _context.Trails.FindAsync(id);
+      if (trail == null)
+      {
+        return NotFound();
+      }
+
+      _context.Trails.Remove(trail);
+      await _context.SaveChangesAsync();
+
+      return trail;
+    }
+
+    private bool TrailExists(int id)
+    {
+      return _context.Trails.Any(e => e.Id == id);
+    }
+  }
 }
