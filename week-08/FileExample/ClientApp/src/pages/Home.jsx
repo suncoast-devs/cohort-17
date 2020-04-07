78e0c5f6c939bec22ebd29a4f934a704855d288a
Mark Dewey <mark.t.dewey@gmail.com>
2020-04-07 10:17:02 -0400

    added dropzone for uploading files

--- week-08/FileExample/ClientApp/src/pages/Home.jsx
+++ week-08/FileExample/ClientApp/src/pages/Home.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import axios from 'axios'
 
 export function Home() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    console.log(acceptedFiles)
+    const fileToUpload = acceptedFiles[0]
+    const formData = new FormData()
+    formData.append('file', fileToUpload)
+    axios
+      .post('/file/upload', formData, {
+        headers: {
+          'content-type': 'multipart/form-data',
+          accept: 'application/json',
+        },
+      })
+      .then(resp => {
+        console.log(resp.data)
+      })
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
   return (
     <div>
       <h1>File Upload Test!</h1>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
     </div>
   )
 }
